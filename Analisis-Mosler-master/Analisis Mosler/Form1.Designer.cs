cfe691a288dfc2abcc54f652850e4cb3a17c0f1f
jcporfirio <106390302+jcporfirio@users.noreply.github.com>
2022-12-02 13:02:41 -0400

    Add files via upload

---
+++ Analisis-Mosler-master/Analisis Mosler/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Analisis_Mosler
+{
+    partial class Mosler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMosler = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Desc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Probabilidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Impacto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Mitigacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Total = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nivel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblNombreRiesgo = new System.Windows.Forms.Label();
+            this.txtNombreRiesgo = new System.Windows.Forms.TextBox();
+            this.btnAgregarRiesgo = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.txtMitigación = new System.Windows.Forms.TextBox();
+            this.lblMitigación = new System.Windows.Forms.Label();
+            this.listProbabilidad = new System.Windows.Forms.ListBox();
+            this.listImpacto = new System.Windows.Forms.ListBox();
+            this.lblProbabilidad = new System.Windows.Forms.Label();
+            this.lblImpacto = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMosler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMosler
+            // 
+            this.dgvMosler.AllowUserToAddRows = false;
+            this.dgvMosler.AllowUserToDeleteRows = false;
+            this.dgvMosler.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvMosler.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvMosler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMosler.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Nombre,
+            this.Desc,
+            this.Probabilidad,
+            this.Impacto,
+            this.Mitigacion,
+            this.Total,
+            this.Nivel});
+            this.dgvMosler.Location = new System.Drawing.Point(4, 199);
+            this.dgvMosler.Name = "dgvMosler";
+            this.dgvMosler.ReadOnly = true;
+            this.dgvMosler.Size = new System.Drawing.Size(681, 140);
+            this.dgvMosler.TabIndex = 0;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            // 
+            // Desc
+            // 
+            this.Desc.HeaderText = "Descripcion";
+            this.Desc.Name = "Desc";
+            this.Desc.ReadOnly = true;
+            // 
+            // Probabilidad
+            // 
+            this.Probabilidad.HeaderText = "Probabilidad";
+            this.Probabilidad.Name = "Probabilidad";
+            this.Probabilidad.ReadOnly = true;
+            // 
+            // Impacto
+            // 
+            this.Impacto.HeaderText = "Impacto";
+            this.Impacto.Name = "Impacto";
+            this.Impacto.ReadOnly = true;
+            // 
+            // Mitigacion
+            // 
+            this.Mitigacion.HeaderText = "Mitigacion";
+            this.Mitigacion.Name = "Mitigacion";
+            this.Mitigacion.ReadOnly = true;
+            // 
+            // Total
+            // 
+            this.Total.HeaderText = "Total";
+            this.Total.Name = "Total";
+            this.Total.ReadOnly = true;
+            // 
+            // Nivel
+            // 
+            this.Nivel.HeaderText = "Nivel";
+            this.Nivel.Name = "Nivel";
+            this.Nivel.ReadOnly = true;
+            // 
+            // lblNombreRiesgo
+            // 
+            this.lblNombreRiesgo.AutoSize = true;
+            this.lblNombreRiesgo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreRiesgo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblNombreRiesgo.Location = new System.Drawing.Point(149, 26);
+            this.lblNombreRiesgo.Name = "lblNombreRiesgo";
+            this.lblNombreRiesgo.Size = new System.Drawing.Size(125, 16);
+            this.lblNombreRiesgo.TabIndex = 1;
+            this.lblNombreRiesgo.Text = "Nombre del Riesgo";
+            // 
+            // txtNombreRiesgo
+            // 
+            this.txtNombreRiesgo.Location = new System.Drawing.Point(152, 44);
+            this.txtNombreRiesgo.Name = "txtNombreRiesgo";
+            this.txtNombreRiesgo.Size = new System.Drawing.Size(136, 20);
+            this.txtNombreRiesgo.TabIndex = 2;
+            // 
+            // btnAgregarRiesgo
+            // 
+            this.btnAgregarRiesgo.Location = new System.Drawing.Point(25, 34);
+            this.btnAgregarRiesgo.Name = "btnAgregarRiesgo";
+            this.btnAgregarRiesgo.Size = new System.Drawing.Size(93, 38);
+            this.btnAgregarRiesgo.TabIndex = 3;
+            this.btnAgregarRiesgo.Text = "Agregar";
+            this.btnAgregarRiesgo.UseVisualStyleBackColor = true;
+            this.btnAgregarRiesgo.Click += new System.EventHandler(this.btnAgregarRiesgo_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(25, 99);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(93, 38);
+            this.btnBorrar.TabIndex = 4;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(318, 42);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(155, 22);
+            this.txtDescripcion.TabIndex = 6;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcion.ForeColor = System.Drawing.Color.Snow;
+            this.lblDescripcion.Location = new System.Drawing.Point(315, 26);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(79, 16);
+            this.lblDescripcion.TabIndex = 5;
+            this.lblDescripcion.Text = "Descripción";
+            // 
+            // txtMitigación
+            // 
+            this.txtMitigación.Location = new System.Drawing.Point(505, 42);
+            this.txtMitigación.Multiline = true;
+            this.txtMitigación.Name = "txtMitigación";
+            this.txtMitigación.Size = new System.Drawing.Size(155, 22);
+            this.txtMitigación.TabIndex = 8;
+            // 
+            // lblMitigación
+            // 
+            this.lblMitigación.AutoSize = true;
+            this.lblMitigación.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMitigación.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblMitigación.Location = new System.Drawing.Point(502, 26);
+            this.lblMitigación.Name = "lblMitigación";
+            this.lblMitigación.Size = new System.Drawing.Size(68, 16);
+            this.lblMitigación.TabIndex = 7;
+            this.lblMitigación.Text = "Mitigación";
+            // 
+            // listProbabilidad
+            // 
+            this.listProbabilidad.FormattingEnabled = true;
+            this.listProbabilidad.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.listProbabilidad.Location = new System.Drawing.Point(152, 101);
+            this.listProbabilidad.Name = "listProbabilidad";
+            this.listProbabilidad.Size = new System.Drawing.Size(140, 30);
+            this.listProbabilidad.TabIndex = 9;
+            // 
+            // listImpacto
+            // 
+            this.listImpacto.FormattingEnabled = true;
+            this.listImpacto.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.listImpacto.Location = new System.Drawing.Point(318, 99);
+            this.listImpacto.Name = "listImpacto";
+            this.listImpacto.Size = new System.Drawing.Size(169, 30);
+            this.listImpacto.TabIndex = 10;
+            // 
+            // lblProbabilidad
+            // 
+            this.lblProbabilidad.AutoSize = true;
+            this.lblProbabilidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProbabilidad.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblProbabilidad.Location = new System.Drawing.Point(149, 83);
+            this.lblProbabilidad.Name = "lblProbabilidad";
+            this.lblProbabilidad.Size = new System.Drawing.Size(85, 16);
+            this.lblProbabilidad.TabIndex = 11;
+            this.lblProbabilidad.Text = "Probabilidad";
+            // 
+            // lblImpacto
+            // 
+            this.lblImpacto.AutoSize = true;
+            this.lblImpacto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImpacto.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblImpacto.Location = new System.Drawing.Point(315, 80);
+            this.lblImpacto.Name = "lblImpacto";
+            this.lblImpacto.Size = new System.Drawing.Size(55, 16);
+            this.lblImpacto.TabIndex = 12;
+            this.lblImpacto.Text = "Impacto";
+            // 
+            // Mosler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(697, 350);
+            this.Controls.Add(this.lblImpacto);
+            this.Controls.Add(this.lblProbabilidad);
+            this.Controls.Add(this.listImpacto);
+            this.Controls.Add(this.listProbabilidad);
+            this.Controls.Add(this.txtMitigación);
+            this.Controls.Add(this.lblMitigación);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnAgregarRiesgo);
+            this.Controls.Add(this.txtNombreRiesgo);
+            this.Controls.Add(this.lblNombreRiesgo);
+            this.Controls.Add(this.dgvMosler);
+            this.Name = "Mosler";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Mosler_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMosler)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMosler;
+        private System.Windows.Forms.Label lblNombreRiesgo;
+        private System.Windows.Forms.TextBox txtNombreRiesgo;
+        private System.Windows.Forms.Button btnAgregarRiesgo;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.TextBox txtMitigación;
+        private System.Windows.Forms.Label lblMitigación;
+        private System.Windows.Forms.ListBox listProbabilidad;
+        private System.Windows.Forms.ListBox listImpacto;
+        private System.Windows.Forms.Label lblProbabilidad;
+        private System.Windows.Forms.Label lblImpacto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Desc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Probabilidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Impacto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Mitigacion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Total;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nivel;
+    }
+}
+
---
+++ Analisis-Mosler-master/Analisis Mosler/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Analisis_Mosler
+{
+    public partial class Mosler : Form {
+
+        //Variables
+
+        private int n = 0; //Este es el numero de filas
+        int nProbabilidad = 0; //Estos son los datos que tomaremos una formula para sacar el total del riesgo
+        int nImpacto = 0; 
+        int Calculo = 0;
+        string vl; //Este es el texto que debe caer dentro de Nivel
+        int id = 0;
+
+        public Mosler(){
+
+            InitializeComponent();
+        }
+
+        private void btnAgregarRiesgo_Click(object sender, EventArgs e){
+
+            //Funcion de añadir nueva fila
+
+            int n = dgvMosler.Rows.Add();
+
+            //ID del riesgo
+
+            for(id = 0; id <= n; id++)
+            {
+                dgvMosler.Rows[n].Cells[0].Value = id + 1;
+            }
+
+            //Para el total, debemos convertir los valores de las ListBox a numeros para luego hacer las operaciones
+
+            //Probabilidad
+            nProbabilidad = Convert.ToInt16(listProbabilidad.Text);
+
+            //Impacto
+            nImpacto = Convert.ToInt16(listImpacto.Text);
+
+            //Con las variables que tenemos ahora, vamos a tomar una multiplicación y lo colocamos en "Total"
+            //El valor minimo es 1 y el maximo es 25, asi que vamos a tomar 5 numeros para cada Nivel de riesgo
+            //A cada nivel de dificultad le asignaremos un color para identificar qué tan grave es sin leer el texto
+
+            Calculo = nProbabilidad * nImpacto;
+
+            if (Calculo >= 1 && Calculo <= 5)
+            {
+                vl = "1-Riesgo muy bajo";
+                dgvMosler.Rows[n].Cells[7].Style.BackColor = Color.GreenYellow; //BackColor es para el color de fondo de la celda
+                dgvMosler.Rows[n].Cells[7].Style.ForeColor = Color.White; //ForeColor es para el color de las letras
+            }
+            if (Calculo >= 6 && Calculo <= 10)
+            {
+                vl = "2-Riesgo bajo";
+                dgvMosler.Rows[n].Cells[7].Style.BackColor = Color.Green;
+                dgvMosler.Rows[n].Cells[7].Style.ForeColor = Color.White;
+            }
+            if (Calculo >= 11 && Calculo <= 15)
+            {
+                vl = "3-Riesgo normal";
+                dgvMosler.Rows[n].Cells[7].Style.BackColor = Color.Orange;
+                dgvMosler.Rows[n].Cells[7].Style.ForeColor = Color.White;
+            }
+            if (Calculo >= 16 && Calculo <= 20)
+            {
+                vl = "4-Riesgo alto";
+                dgvMosler.Rows[n].Cells[7].Style.BackColor = Color.OrangeRed;
+                dgvMosler.Rows[n].Cells[7].Style.ForeColor = Color.White;
+
+            }
+            if (Calculo >= 21 && Calculo <= 25)
+            {
+                vl = "5-Riesgo muy alto";
+                dgvMosler.Rows[n].Cells[7].Style.BackColor = Color.Red;
+                dgvMosler.Rows[n].Cells[7].Style.ForeColor = Color.White;
+            }
+
+            //Colocamos la informacion
+
+            dgvMosler.Rows[n].Cells[1].Value = txtNombreRiesgo.Text;
+            dgvMosler.Rows[n].Cells[2].Value = txtDescripcion.Text;
+            dgvMosler.Rows[n].Cells[3].Value = listProbabilidad.Text;
+            dgvMosler.Rows[n].Cells[4].Value = listImpacto.Text;
+            dgvMosler.Rows[n].Cells[5].Value = txtMitigación.Text;
+            dgvMosler.Rows[n].Cells[6].Value = Calculo;
+            dgvMosler.Rows[n].Cells[7].Value = vl;
+
+            //Limpiar los txt
+            txtNombreRiesgo.Clear();
+            txtDescripcion.Clear();
+            txtMitigación.Clear();
+        }
+
+        private void Mosler_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (n != -1){
+
+                dgvMosler.Rows.RemoveAt(n);
+            }
+        }
+    }
+}
